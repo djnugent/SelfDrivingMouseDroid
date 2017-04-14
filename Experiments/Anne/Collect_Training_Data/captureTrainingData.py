38386539a670b6d4efe249d673919cb7b2b8963a
atesar <atesar@iastate.edu>
2017-04-14 15:58:06 -0500

    Replace captureTrainingData.py

--- Experiments/Anne/Collect_Training_Data/captureTrainingData.py
+++ Experiments/Anne/Collect_Training_Data/captureTrainingData.py
@@ -58,7 +58,7 @@ if __name__ == "__main__" :
             #if cv2.waitKey(1) & 0xFF == ord('q'):
              #   break
 
-             time.sleep(.2);
+            time.sleep(.2)
 
     finally:
         print ("Done!")
