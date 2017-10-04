d84abd34333ac5471868b3ae1e72cd33f5e1d1d0
atesar <atesar@iastate.edu>
2017-10-04 12:32:47 -0500

    Upload New File

---
+++ Experiments/Anne/Training Tool/fetchMetadata.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+def fetch():
+    print("Fetching Metadata")
+    directory = "/remote/rs/ecpeprime"
+
+    data = [];
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
+            if file == "metadata.txt":
+                f = open(os.path.join(subdir, file), 'r')
+                alltext = f.read()
+                entries = alltext.split("~")
+                json_entry = {"recordedBy":entries[0],
+                              "location":entries[1],
+                              "batch_size":entries[2],
+                              "obstacles":entries[3],
+                              "pedestrians":entries[4],
+                              "tags":entries[5],
+                              "notes":entries[6],
+                              "date":entries[7]}
+                data.append(json_entry)
+    #print(json.dumps(data))
+    return json.dumps(data)                
