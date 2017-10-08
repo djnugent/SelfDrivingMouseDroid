7ad2d1dc60f52d9ebc82bc41f5b24b394f6f6436
atesar <atesar@iastate.edu>
2017-10-07 20:51:49 -0500

    Replace hello_flask.py

--- Experiments/Anne/Training Tool/hello_flask.py
+++ Experiments/Anne/Training Tool/hello_flask.py
@@ -19,8 +19,12 @@ resultNumbers = []
 def index():
     """ Displays the index page accessible at '/'
     """
-    return flask.render_template('index.html', metadata = metadata)
- 
+    if (len(resultNumbers) > 0):
+        return flask.render_template('index0.html', metadata = resultNumbers)
+    else:
+        return flask.render_template('index.html', metadata = metadata)
+
+
 @APP.route('/train', methods=['POST'])
 def train():
     if request.method == "POST":
@@ -31,7 +35,9 @@ def train():
                print(data)
         trainingThread = threading.Thread(target = trainOn, args=(), kwargs={'trainingDatasets':content['trainingDatasets'], 'resultNumbers':resultNumbers})
         trainingThread.start()
-        return json.dumps({'status':'ok'})
+
+        currentlyTraining = True
+        return json.dumps({'status':'OK'})
 
 @APP.route('/refetch', methods=['POST'])
 def refetch():
