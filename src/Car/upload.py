6784e69a367728bb521bd5514410ef113e251fca
odroid <djnugent@iastate.edu>
2017-10-13 01:20:40 +0000

    Added upload script, need to add move and delete functionality

--- src/Car/upload.py
+++ src/Car/upload.py
@@ -1,8 +1,46 @@
-# todo dont upload incomplete batches
-# delete after upload
+
+# TODO recursive move files and delete after transfer
+
+import os
+import re
+
+# Regex for extracting batchsize from meta data
+p = re.compile("Batch Size: (?:\d*)?\d+")
+
 
 directory = '/remote/rs/ecpeprime/training_data'
+local_directory = '/home/odroid/training_data_tmp'
 
-    # Remove incomplete batch
-    if (frame_count < batch_size - 1):
-        shutil.rmtree(directory + "/" + run_name + "/" + str(batch_num))
+# List runs
+path, runs, files = os.walk(local_directory).__next__()
+print(">> Found {} runs".format(len(runs)))
+
+# Iterate through each run
+for i,run in enumerate(runs):
+	print(">> Run",i)
+	# Extract batch size from metadata
+	metadata = open(local_directory + "/" + run + "/metadata.txt","r")
+	line = metadata.read()
+	batch_size = int(p.search(line).group().replace("Batch Size: ",""))
+
+	# Detect batches in each run
+	path, batches, files = os.walk(local_directory + "/" + run).__next__()
+	print("   --Found {} batches".format(len(batches)))
+	
+	for j,batch in enumerate(batches):
+		# Check for complete batch
+		path, dirs, files = os.walk(local_directory + "/" + run + "/" + batch).__next__()
+		# valid batch, move to server
+		if len(files) - 1 == batch_size:	
+			print("   --Uploading batch {}".format(j))
+			# mv batch
+		# incomplete batch, skip
+		else:
+			print("   --Batch {} incomplete. Skipping".format(j))
+
+	# delete run from local directory after successful transfer
+	print("   --Run {} transfer complete. Removing".format(i))
+
+print("Upload complete")
+			
+
