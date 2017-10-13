a7af94a44377cf25fc6b3bf179b9a8c75462858b
atesar <atesar@iastate.edu>
2017-10-13 12:13:14 -0500

    Upload New File

---
+++ src/Server/Training_Tool/flask_app.py
@@ -0,0 +1,45 @@
+import flask
+from flask import request, jsonify
+import json
+import sys
+import threading
+
+from dataManage import *
+from trainNetwork import *
+
+# Create the application.
+APP = flask.Flask(__name__)
+
+resultNumbers = []
+
+@APP.route('/')
+def index():
+    """ Displays the index page accessible at '/'
+    """
+    if (len(resultNumbers) > 0):
+        return flask.render_template('index0.html', data = resultNumbers)
+    else:
+        metadata = fetch()
+        return flask.render_template('index.html', data = metadata)
+
+
+@APP.route('/train', methods=['POST'])
+def train():
+    if request.method == "POST":
+        #print("TRAINING")
+        content = request.get_json()
+
+        trainingThread = threading.Thread(target = trainOn, args=(), kwargs={'modelData':content['modelData'], 'resultNumbers':resultNumbers})
+        trainingThread.start()
+        return json.dumps({"status":"ok"})
+
+@APP.route('/delete', methods=['POST'])
+def delete():
+   if request.method == "POST":
+      content = request.get_json()
+      print(content["dataset"])
+      deleteDataset(content['dataset'])
+      return json.dumps({"status":"ok"})
+
+if __name__ == "__main__":
+	APP.run(host="0.0.0.0", port="8080")
